ef9205b66019f6780af83fede720534fbc36f0a0
orcap23 <91634581+orcap23@users.noreply.github.com>
2022-02-13 14:42:11 -0800

    overhaul of graph.py but not in a bad way

--- graph.py
+++ graph.py
@@ -8,15 +8,29 @@ class Graph:
     def __init__(self, vertices) -> None:
         self.V = vertices
         self.graph = []
-        self.node = []
-        self.MinST = []
 
     def addEdge(self, s, d, w):
         self.graph.append([s, d, w])
 
-    def addNode(self, value):
-        self.node.append(value)
+     # recursive seraching function
+    def find(self, parent, obj):
+        if parent[obj] == obj:
+            return obj
+        else:
+            return self.find(parent, parent[obj])
+
+    def union(self, parent, rank, a, b):
+        aroot = self.find(parent, a)
+        broot = self.find(parent, b)
+
+        if rank[aroot] < rank[broot]:
+            parent[aroot] = broot
+        elif rank[aroot] > rank[broot]:
+            parent[broot] = aroot
+        else:
+            parent[aroot] = broot
+            rank[aroot] += 1
     
     def printSolution(self, s, d, w):
-        for s, d, w in self.MinST:
+        for s, d, w in self.graph:
             print(f'{s} - {d} : {w}')
